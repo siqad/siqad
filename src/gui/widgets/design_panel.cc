f339cdbd6ffd15ba9a808678fdefff6d91681253
samuelngsh <samuelngsh@gmail.com>
2017-07-13 14:29:03 -0700

    Fixed Esc sidebar mode change, and updated TODO list

--- src/gui/widgets/design_panel.cc
+++ src/gui/widgets/design_panel.cc
@@ -546,6 +546,7 @@ void gui::DesignPanel::keyReleaseEvent(QKeyEvent *e)
   }
   else{
     switch(e->key()){
+      case Qt::Key_Escape:
         // deactivate current tool
         if(tool_type != gui::DesignPanel::SelectTool){
           // emit signal to be picked up by application.cc
