c246040627c32337123e568b0910dddc259eb78c
samuelngsh <samuelngsh@gmail.com>
2017-07-14 14:23:10 -0700

    ... delete all reference to tmp config

--- src/settings/settings.cc
+++ src/settings/settings.cc
@@ -84,7 +84,6 @@ QSettings *settings::AppSettings::m_defs()
 {
   qDebug("Loading default application settings...");
   QSettings *S = new QSettings("src/settings/defaults/app_settings.ini", QSettings::IniFormat);
-  //QSettings *S = new QSettings("tmp/settings/defaults/app_settings.ini", QSettings::IniFormat);
 
   // overwrites existing default values with same keys... no check
 
@@ -106,7 +105,6 @@ QSettings* settings::GUISettings::m_defs()
 {
   qDebug("Loading default gui settings...");
   QSettings *S = new QSettings("src/settings/defaults/gui_settings.ini", QSettings::IniFormat);
-  //QSettings *S = new QSettings("tmp/settings/defaults/gui_settings.ini", QSettings::IniFormat);
 
   S->setValue("MWIN/size", QSize(1400, 800));
   S->setValue("TBAR/mh", 60);
