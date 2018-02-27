cb5b433589d25301a25a568bf87b4939407f3a75
NathanChiu <nathan.chiu94@gmail.com>
2018-02-27 15:54:52 -0800

    Begin doxygen documentation. Users should generate their own documentation by running 'doxygen Doxyfile' in the root directory. Once finished, documentation can be viewed by opening index.html inside of doxygen/html/ with a web browser.

--- src/gui/widgets/primitives/electrode.h
+++ src/gui/widgets/primitives/electrode.h
@@ -1,10 +1,11 @@
-// @file:     electrode.h
-// @author:   Nathan
-// @created:  2017.10.27
-// @editted:  2018.01.17 - Nathan
-// @license:  GNU LGPL v3
-//
-// @desc:     Electrode objects for creation, deletion, and moving of electrodes.
+/** @file:     electrode.h
+ *  @author:   Nathan
+ *  @created:  2017.10.27
+ *  @editted:  2018.01.17 - Nathan
+ *  @license:  GNU LGPL v3
+ *
+ *  @brief:     Function prototypes for the Electrode object.
+ */
 
 #ifndef _GUI_PR_ELECTRODE_H_
 #define _GUI_PR_ELECTRODE_H_
@@ -20,25 +21,19 @@ namespace prim{
   class Electrode: public prim::Item
   {
   public:
-
-    // constructor, create an electrode given two points
+    //! constructor, creates an electrode given two points.
     Electrode(int lay_id, QPointF point1, QPointF point2);
-    // constructor, create an electrode using the data found in a design file.
+    //! constructor, creates an electrode from the design file.
     Electrode(QXmlStreamReader *ls, QGraphicsScene *scene);
-
-
-    // destructor
+    //! destructor
     ~Electrode(){}
-
-    //Electrodes will have two types, clocked (that will change over time) and fixed
+    //! Clock types will vary over time. Fix types are static.
     enum ElectrodeType{Clock, Fix};
-
     ElectrodeType electrode_type;
-
-    //initializer
+    // Initializer for initial attribute values.
     void initElectrode(int lay_id, QPointF point1_in, QPointF point2_in, double potential_in=0, int electrode_type_in=0);
 
-    //setters
+    // sets the electrode potential to givenPotential.
     void setPotential(double givenPotential);
 
     // accessors
@@ -51,7 +46,7 @@ namespace prim{
     qreal getDepth(void){return elec_depth;}
     double getPotential(void) const {return potential;}
 
-    //! updates the electrode with its new location after moving it with the mouse.
+    //! Updates the electrode with its new location. Call this after moving the electrode.
     void updatePoints(QPointF);
 
     // inherited abstract method implementations
@@ -59,15 +54,16 @@ namespace prim{
     void paint(QPainter *, const QStyleOptionGraphicsItem *, QWidget *) Q_DECL_OVERRIDE;
     Item *deepCopy() const;
 
-    // SAVE LOAD
+    // saving to design
     virtual void saveItems(QXmlStreamWriter *) const;
-  //
+
   protected:
+
+    // Mouse events
     virtual void mousePressEvent(QGraphicsSceneMouseEvent *e) Q_DECL_OVERRIDE;
     virtual void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *e) Q_DECL_OVERRIDE;
 
   private:
-
     // construct static variables
     void constructStatics();
 
@@ -75,7 +71,6 @@ namespace prim{
     QPointF point1;
     QPointF point2;
     QPointF top_left; //top left point, since the two points given could be any two opposite points
-
     double potential = 0;
     qreal elec_width;
     qreal elec_height;
@@ -91,4 +86,3 @@ namespace prim{
 } // end prim namespace
 
 #endif
-//////
--- src/gui/widgets/primitives/pot_plot.h
+++ src/gui/widgets/primitives/pot_plot.h
@@ -1,10 +1,11 @@
-// @file:     pot_plot.h
-// @author:   Nathan
-// @created:  2017.10.27
-// @editted:  2018.01.17 - Nathan
-// @license:  GNU LGPL v3
-//
-// @desc:     pot_plot objects for creation of potential plots on design panel.
+/** @file:     pot_plot.h
+ *  @author:   Nathan
+ *  @created:  2017.10.27
+ *   @editted:  2018.01.17 - Nathan
+ *  @license:  GNU LGPL v3
+ *
+ *  @brief:     Function prototypes for the PotPlot object.
+ */
 
 #ifndef _GUI_PR_POT_PLOT_H_
 #define _GUI_PR_POT_PLOT_H_
@@ -20,10 +21,11 @@ namespace prim{
   class PotPlot: public prim::Item
   {
   public:
-    // constructor, create an electrode given two points
+    //! constructor, create a PotPlot given a QPixmap of the plot,
+    //! and a QRectF to contain it.
     PotPlot(int lay_id, QPixmap potential_plot, QRectF graph_container);
 
-    // destructor
+    //! destructor
     ~PotPlot(){}
 
     //initializer
