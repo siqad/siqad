652915dae9ffc6322c7ff38dd2fb1c847c03b849
Nathan Chiu <nathan.chiu94@gmail.com>
2018-07-13 12:52:35 -0700

    Start replacing electrode with polygonal electrodes

---
+++ src/gui/widgets/primitives/electrode_poly.cc
@@ -0,0 +1,11 @@
+// @file:     electrode_poly.cc
+// @author:   Nathan
+// @created:  2017.10.27
+// @editted:  2017.10.27 - Nathan
+// @license:  GNU LGPL v3
+//
+// @desc:     ElectrodePoly class for the functionality of polygonal electrodes
+
+#include <algorithm>
+#include "electrode_poly.h"
+#include "src/settings/settings.h"
---
+++ src/gui/widgets/primitives/electrode_poly.h
@@ -0,0 +1,23 @@
+/** @file:     electrode_poly.h
+ *  @author:   Nathan
+ *  @created:  2017.10.27
+ *  @editted:  2018.01.17 - Nathan
+ *  @license:  GNU LGPL v3
+ *
+ *  @brief:     Function prototypes for the ElectrodePoly object.
+ */
+
+#include <QtWidgets>
+#include "resizablerect.h"
+
+namespace prim{
+
+  class Layer;
+
+  class ElectrodePoly: public QPolygonF
+  {
+  public:
+    ElectrodePoly(int lay_id, const QPolygonF);
+  }
+
+} //end prim namespace
